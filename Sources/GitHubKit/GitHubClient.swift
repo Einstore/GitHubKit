291b58a19dbbdbcfbf3b43e66c04fcd627feec92
pmmlo <productmichael@gmail.com>
2021-07-20 22:33:31 -0700

    Add delete overload func and protocol to allow file delete - API current returns a content/file and commit objects.

--- Sources/GitHubKit/GitHubClient.swift
+++ Sources/GitHubKit/GitHubClient.swift
@@ -10,6 +10,7 @@ public protocol GitHubClient {
     func put<C, E>(path: String, post: E) throws -> EventLoopFuture<C?> where C: Decodable, E: Encodable
     func patch<C, E>(path: String, post: E) throws -> EventLoopFuture<C?> where C: Decodable, E: Encodable
     func delete(path: String) throws -> EventLoopFuture<Void>
+    func delete<C, E>(path: String, post: E) throws -> EventLoopFuture<C?> where C: Decodable, E: Encodable
     func get(file path: String) throws -> EventLoopFuture<Data?>
     func redirect(file path: String, status: HTTPResponseStatus) throws -> EventLoopFuture<String>
     func download(org: String, repo: String, ref: String, format: GitHub.Format) throws -> EventLoopFuture<String>
